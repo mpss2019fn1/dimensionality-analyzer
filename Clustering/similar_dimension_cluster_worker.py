5666e9073d97dc90400dfa171ac32aad72f3014a
Johannes Schneider <johannes.schneider@student.hpi.de>
2019-07-15 15:41:28 +0200

    improve performance by defining a minimum cluster size

--- Clustering/similar_dimension_cluster_worker.py
+++ Clustering/similar_dimension_cluster_worker.py
@@ -18,14 +18,15 @@ class SimilarDimensionClusterWorker(Process):
         self._embedding: KeyedVectors = embedding
         self._work: Queue = work
         self._clusters: List[Cluster] = []
+        self._minimum_cluster_size: int = max(10, len(self._embedding.vectors) // (self._embedding.vector_size * 10))
 
         # state for cluster extraction
         self._dimension: int = 0
         self._sorted_values: List[float] = []
         self._sorted_labels: List[str] = []
         self._tolerance: float = 0.0
-        self._biggest_cluster: List[str] = []
-        self._current_cluster: List[str] = []
+        self._biggest_cluster: Tuple[int, int] = (0, 0)
+        self._current_cluster: Tuple[int, int] = (0, 0)
         self._current_start_index: int = 0
         self._current_end_index: int = 0
 
@@ -36,18 +37,19 @@ class SimilarDimensionClusterWorker(Process):
         try:
             while not self._work.empty():
                 self._dimension = self._work.get_nowait()
-                logging.info("working on dimension %d", self._dimension)
+                logging.info(f"[DIMENSION-{self._dimension}] begin")
 
                 cluster: Cluster = self._extract_cluster()
+                if len(cluster) < self._minimum_cluster_size:
+                    continue
 
-                logging.info("%s", cluster)
-
+                logging.info(f"[DIMENSION-{self._dimension}] => cluster.len: {len(cluster)}")
                 self._clusters.append(cluster)
 
         except Empty:
             pass
 
-        logging.info("no more work to do. stopping thread", self.name)
+        logging.info("no more work to do. stopping process", self.name)
 
     def _extract_cluster(self) -> Cluster:
         cluster: Cluster = Cluster(self._dimension, self._dimension)
@@ -59,60 +61,80 @@ class SimilarDimensionClusterWorker(Process):
 
         self._sorted_values = [x[0] for x in sorted_tuples]
         self._sorted_labels = [x[1] for x in sorted_tuples]
+        self._tolerance = statistics.mean(self._sorted_values)
         self._create_biggest_cluster()
 
-        for label in self._biggest_cluster:
-            cluster.add(label)
+        logging.info(f"[DIMENSION-{self._dimension}] done")
+
+        if self._len(self._biggest_cluster) < self._minimum_cluster_size:
+            return cluster
+
+        for label_index in range(self._biggest_cluster[0], self._biggest_cluster[1]):
+            cluster.add(self._sorted_labels[label_index])
 
         return cluster
 
     def _create_biggest_cluster(self) -> None:
-        self._tolerance = statistics.mean(self._sorted_values)
-        self._biggest_cluster.clear()
+        self._biggest_cluster = (0, 0)
         self._current_start_index = 0
 
-        start_time: float = time.perf_counter()
-        create_current_execution_times: List[float] = []
-        find_next_execution_times: List[float] = []
+        execution_times: List[float] = []
+        log_interval: int = len(self._sorted_values) // 100
 
-        while self._start() < len(self._sorted_values):
-            create_current_execution_times.append(SimilarDimensionClusterWorker._profile(self._create_current_cluster))
-            find_next_execution_times.append(SimilarDimensionClusterWorker._profile(self._find_next_start_index))
+        while self._current_start_index < len(self._sorted_values) - 1:
+            start_time: float = time.perf_counter()
+            self._create_current_cluster()
 
-            if len(self._current_cluster) > len(self._biggest_cluster):
+            if self._len(self._current_cluster) > self._len(self._biggest_cluster):
                 self._biggest_cluster = self._current_cluster
 
-            if len(create_current_execution_times) % 5000 == 0:
+            execution_times.append(time.perf_counter() - start_time)
+
+            if len(execution_times) % log_interval == 0:
                 progression: float = self._current_start_index / len(self._sorted_values)
-                logging.info(f"[DIMENSION-{self._dimension}]\t{'%06.2f%%' % (progression * 100.0)}")
-                logging.info(f"[DIMENSION-{self._dimension}]\t\t{'%06.4fs' % (time.perf_counter() - start_time)}")
-                logging.info(f"[DIMENSION-{self._dimension}]\t\t<<_create_current_cluster>>: "
-                             f"avg: {'%06.4fs' % statistics.mean(create_current_execution_times)}; "
-                             f"tot: {'%06.4fs' % sum(create_current_execution_times)}")
-                logging.info(f"[DIMENSION-{self._dimension}]\t\t<<_find_next_start_index>>: "
-                             f"avg: {'%06.4fs' % statistics.mean(find_next_execution_times)}; "
-                             f"tot: {'%06.4fs' % sum(find_next_execution_times)}")
+                total_time: float = sum(execution_times)
+                logging.info(f"[DIMENSION-{self._dimension}]\t{'%06.2f%%' % (progression * 100.0)} "
+                             f"avg: {'%06.4fs' % (total_time / len(execution_times))}; "
+                             f"tot: {'%06.4fs' % total_time}; "
+                             f"<<_biggest_cluster.len>>: {self._len(self._biggest_cluster)}")
 
-                create_current_execution_times.clear()
-                find_next_execution_times.clear()
-                start_time = time.perf_counter()
-
-    @staticmethod
-    def _profile(method: FunctionType, *arguments: Any) -> float:
-        start_time: float = time.perf_counter()
-        method(*arguments)
-        end_time: float = time.perf_counter()
-
-        return end_time - start_time
+                execution_times.clear()
 
     def _create_current_cluster(self) -> None:
-        self._current_cluster = [self._sorted_labels[self._current_start_index]]
-        for self._current_end_index in range(self._current_start_index + 1, len(self._sorted_values)):
+        self._current_cluster = (0, 0)
+        self._current_end_index = min(len(self._sorted_values) - 1,
+                                      self._current_start_index + self._min_len())
 
-            if self._end() - self._start() > self._tolerance:
-                break
+        if self._end() - self._start() > self._tolerance:
+            self._current_start_index += 1
+            return
 
-            self._current_cluster.append(self._sorted_labels[self._current_end_index])
+        while self._current_end_index < len(self._sorted_values) - 1:
+            self._current_end_index = min(len(self._sorted_values) - 1,
+                                          self._current_end_index + self._min_len())
+
+            difference: float = self._end() - self._start()
+
+            if difference < self._tolerance:
+                continue
+
+            if difference == self._tolerance:
+                while difference == self._tolerance and self._current_end_index < len(self._sorted_values) - 1:
+                    self._current_end_index += min(len(self._sorted_values) - 1,
+                                                   self._current_end_index + 1)
+                    difference = self._end() - self._start()
+
+            else:  # difference > self._tolerance
+                while difference > self._tolerance:
+                    self._current_end_index -= 1
+                    difference = self._end() - self._start()
+
+                self._current_end_index += 1  # add one, because the end_index is excluded from this cluster
+
+            break
+
+        self._current_cluster = (self._current_start_index, self._current_end_index)
+        self._find_next_start_index()
 
     def _find_next_start_index(self) -> None:
         if self._current_end_index - self._current_start_index < 2:
@@ -131,3 +153,10 @@ class SimilarDimensionClusterWorker(Process):
 
     def _end(self) -> float:
         return self._sorted_values[self._current_end_index]
+
+    @staticmethod
+    def _len(cluster: Tuple[int, int]) -> int:
+        return cluster[1] - cluster[0]
+
+    def _min_len(self) -> int:
+        return max(self._minimum_cluster_size, self._len(self._biggest_cluster))
